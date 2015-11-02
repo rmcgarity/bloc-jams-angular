ec5585d91b3dbd2aceed0d6e27cc4ef697c8c39b
rmcgarity <ralph_mcgarity@yahoo.com>
2015-11-02 08:00:47 -0600

    Interrim, not final

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,26 @@
-var ralphModule = angular.module('ralphApp', []);
+var ralphModule = angular.module('ralphApp', ['ui.router']);
+
+ralphModule.config(function($stateProvider, $locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+    
+    $urlRouterProvider.otherwise("landing")
+    $stateProvider
+        .state('landing', {
+            url: '/landing',
+            controller: 'Landing.contoller',
+            templateUrl: '../templates/landing.html'
+        })
+        .state('collection', {
+            url: '/collection',
+            controller: 'Collection.contoller',
+            templateUrl: '../templates/collection.html'
+        })
+        .state('album', {
+            url: '/album',
+            controller: 'Album.controller',
+            templateUrl: '../templates/album.html'
+        })
+});
