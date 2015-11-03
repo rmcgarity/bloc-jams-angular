01dfabffeda67364af6ce5435db4f2eef95e29db
rmcgarity <ralph_mcgarity@yahoo.com>
2015-11-03 10:19:48 -0600

    Closer to working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,26 +1,38 @@
 var ralphModule = angular.module('ralphApp', ['ui.router']);
 
+ralphModule.controller("LandingController", function($scope) {
+    // Placeholder to check program behavior
+    alert("Hello World from Landing!");
+})
+ralphModule.controller("CollectionController", function($scope) {
+    // Placeholder to check program behavior
+    alert("Hello World from Collection!");
+})
+ralphModule.controller("AlbumController", function($scope) {
+    // Placeholder to check program behavior
+    alert("Hello World from Album!");
+})
+
 ralphModule.config(function($stateProvider, $locationProvider) {
     $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
     });
-    
-    $urlRouterProvider.otherwise("landing")
+    // $locationProvider.otherwise("/landing")
     $stateProvider
         .state('landing', {
             url: '/landing',
-            controller: 'Landing.contoller',
+            controller: 'LandingController',
             templateUrl: '../templates/landing.html'
         })
         .state('collection', {
             url: '/collection',
-            controller: 'Collection.contoller',
+            controller: 'CollectionController',
             templateUrl: '../templates/collection.html'
         })
         .state('album', {
             url: '/album',
-            controller: 'Album.controller',
+            controller: 'AlbumController',
             templateUrl: '../templates/album.html'
         })
 });
